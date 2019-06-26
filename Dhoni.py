853e4f385bc9881eae052221aa866b2599979f52
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-06-26 15:55:27 +0530

    Create Dhoni.py

---
+++ Dhoni.py
@@ -0,0 +1,15 @@
+  #ry
+   n = int(input())
+  list_b = []
+  a = n//2 + n
+ for i in range(1,n+1):
+   if i%2==0:
+     list_b.append(i)
+ for i in range(1,n+1):
+   if i%2!=0:
+     list_b.append(i)
+ for i in range(1,n+1):
+   if i%2==0:
+     list_b.append(i)
+ print(a)
+ print(*list_b)
